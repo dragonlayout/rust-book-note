437018002d58fcd97df93ec4485101dc004a6e11
chenlong <dragonlayoutt@gmail.com>
2021-02-03 13:36:16 +0800

    update: add minigrep package

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::{env, process};
+use minigrep::Config;
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing argument: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
